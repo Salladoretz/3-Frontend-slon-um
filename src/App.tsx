f0467697cbfe8ecce5389326636cbac9c7cb9f9e
Salladoretz <salladoretz@yandex.ru>
2022-08-01 18:03:21 +1000

    url

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='slon-um' element={<AppDefault />} />
+        <Route path='3-Frontend-slonUm' element={<AppDefault />} />
         <Route path='slon-um-alt' element={<AppAlt />} />
       </Routes>
     </div>
