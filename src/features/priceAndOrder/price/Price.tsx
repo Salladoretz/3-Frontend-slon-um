3167d83a61a20dd5e4ba769213a42df549ca4621
Salladoretz <salladoretz@yandex.ru>
2022-08-01 21:44:24 +1000

    svg fix

--- src/features/priceAndOrder/price/Price.tsx
+++ src/features/priceAndOrder/price/Price.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import css from './Price.module.scss'
 import { useAppSelector } from '../../../app/hooks'
+import PriceTop from '../../../assets/PriceTop.svg'
 import VK from '../../../assets/VK.svg'
 import OK from '../../../assets/OK.svg'
 
@@ -12,6 +13,11 @@ const Price = () => {
     return (
         <div className={css.price}>
             <div className={css.price__top}>
+                <svg width="292" height="110" viewBox="0 0 292 110" fill="" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M281.07 14.7005H292L281.07 0V14.7005Z" fill="#0E8958" />
+                    <path d="M10.9305 14.7005H0L10.9305 0V14.7005Z" fill="#0E8958" />
+                    <path d="M281.07 14.7005V0H10.9305V14.7005V110L146 74.5161L281.07 110V14.7005Z" fill="var(--mainAccentColor)" />
+                </svg>
                 <p>Скидка за репост</p>
             </div>
             <div className={css.price__middle}>
