d46d3a21d5f46d03852c7c608f5a5fbd40e9e343
Salladoretz <salladoretz@yandex.ru>
2022-08-01 16:15:03 +1000

    back

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import './App.scss'
-import AppDefault from './AppDefault'
-import AppAlt from './AppAlt'
-import { Routes, Route } from 'react-router-dom'
+import Main from './features/main/Main'
+import Benefits from './features/benefits/Benefits'
+import Plan from './features/plan/Plan'
+import Sertificate from './features/sertificate/Sertificate'
+import PriceAndOrder from './features/priceAndOrder/PriceAndOrder'
+import QA from './features/qa/QA'
+import Subscribe from './features/subscribe/Subscribe'
+import Footer from './features/footer/Footer'
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='slon-um' element={<AppDefault />} />
-        <Route path='slon-um-alt' element={<AppAlt />} />
-      </Routes>
-      <AppDefault />
+    <div className="App">
+      <Main />
+      <Benefits />
+      <Plan />
+      <Sertificate />
+      <PriceAndOrder />
+      <QA />
+      <Subscribe />
+      <Footer />
     </div>
   )
 }
